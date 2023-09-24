9c8ce5778ef5ecc027295156b33eaca09c95ec51
Kat Nix <deutschkat29@gmail.com>
2023-09-24 14:11:19 -0500

    microservice managed

---
+++ src/test/java/com/example/microservice/manage/MicroserviceManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
